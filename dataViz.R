5bfa3037aec4914c0061ad0df33460dbc65f28d7
NicoJaws23 <nico.jaworski@utexas.edu>
2025-04-28 13:16:04 -0500

    4/28 edits

--- dataViz.R
+++ dataViz.R
@@ -1,5 +1,6 @@
 #Data visualizations code
 library(adehabitatHR)
+library(leaflet)
 library(leafsync)
 library(mapview)
 library(tidyverse)
@@ -67,7 +68,7 @@ elev <- function(df, map, hr, res){
   
   ev <- mapview(elevation_clipped, layer.name = "Average Elevation", col.regions = terrain.colors(20), legend = TRUE, na.color = NA)
   evMap <- map + ev
-  return(evMap)
+  return(list(evMap = evMap, evClip = elevation_clipped))
   
 }
 
@@ -76,4 +77,35 @@ ev15 <- elev(d15, hr15$map, hr15$homerange, 150)
 ev16 <- elev(d16, hr16$map, hr16$homerange, 150)
 ev17 <- elev(d17, hr17$map, hr17$homerange, 150)
 ev18 <- elev(d18, hr18$map, hr18$homerange, 150)
-sync(ev14, ev15, ev16, ev17, ev18, ncol = 1)
+sync(ev14$evMap, ev15$evMap, ev16$evMap, ev17$evMap, ev18$evMap, ncol = 1)
+
+#Adding Icons
+mountain <- makeIcon(iconUrl = "https://www.pngmart.com/files/23/Cartoon-Mountains-PNG-Isolated-HD.png", iconWidth = 20, iconHeight = 20)
+tree <- makeIcon(iconUrl = "https://img.pikbest.com/png-images/tree-clipart-cartoon-jungle-vector-green-big-tree_5856321.png!sw800", iconWidth = 20, iconHeight = 20)
+hill <- makeIcon(iconUrl = "https://png.pngtree.com/png-vector/20220929/ourmid/pngtree-green-hill-ground-vector-clipart-png-image_6221576.png", iconWidth = 20, iconHeight = 20)
+evIcon <- function(evClipped, hr, icon1, icon2, icon3, trailsDF, riverDF, maxThreshold, minThreshold){
+  evPoints <- as.points(evClipped)
+  evPoints <- project(evPoints, "EPSG:4326")
+  evDF <- as.data.frame(evPoints, geom = "xy", na.rm = TRUE, cellvalues = TRUE)
+  evDF <- evDF|>
+    mutate(class = case_when(
+      evDF$mean >= maxThreshold ~ "mountain",
+      evDF$mean < maxThreshold & evDF$mean >= minThreshold ~ "hill",
+      evDF$mean < minThreshold ~ "tree"
+    ))
+  map <- leaflet() |>
+    addProviderTiles(providers$Esri.WorldTopoMap) |>
+    addPolygons(data = st_transform(hr, 4326), color = "tan4", weight = 2, fillOpacity = 0.3, group = "Home Range") |>
+    addPolylines(data = st_transform(trailsDF, 4326), color = "orange", weight = 2, group = "Trails") |>
+    addPolylines(data = st_transform(riverDF, 4326), color = "blue", weight = 2, group = "River") |>
+    addMarkers(data = filter(evDF, class == "mountain"), lng = ~x, lat = ~y, icon = icon1) |>
+    addMarkers(data = filter(evDF, class == "hill"), lng = ~x, lat = ~y, icon = icon2) |>
+    addMarkers(data = filter(evDF, class == "tree"), lng = ~x, lat = ~y, icon = icon3) |>
+    addLayersControl(overlayGroups = c("Home Range", "Trails", "River"), options = layersControlOptions(collapsed = FALSE))
+  return(map)
+}
+(i14 <- evIcon(ev14$evClip, hr14$homerange, icon1 = mountain, icon2 = hill, icon3 = tree, trails, river, maxThreshold = 260, minThreshold = 240))
+(i15 <- evIcon(ev15$evClip, hr15$homerange, icon1 = mountain, icon2 = hill, icon3 = tree, trails, river, maxThreshold = 260, minThreshold = 240))
+(i16 <- evIcon(ev16$evClip, hr16$homerange, icon1 = mountain, icon2 = hill, icon3 = tree, trails, river, maxThreshold = 260, minThreshold = 240))
+(i17 <- evIcon(ev17$evClip, hr17$homerange, icon1 = mountain, icon2 = hill, icon3 = tree, trails, river, maxThreshold = 260, minThreshold = 240))
+(i18 <- evIcon(ev18$evClip, hr18$homerange, icon1 = mountain, icon2 = hill, icon3 = tree, trails, river, maxThreshold = 260, minThreshold = 240))
